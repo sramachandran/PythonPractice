fe4e748ab527178cdc3ec6d6ecbb8838ddbc842a
sramachandran <subalakshmir@yahoo.com>
2020-02-05 16:34:03 -0800

    commiting double_char

---
+++ double_char.py
@@ -0,0 +1,7 @@
+#Create a function that takes a string and returns a string in which each character is repeated once.
+
+l = list()
+txt='string'
+for i in txt:
+    l.append(2*i)
+print("".join(l))
